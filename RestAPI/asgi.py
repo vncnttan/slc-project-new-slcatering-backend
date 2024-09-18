b20813608637d54989e32200eaf3ca3b8127ba3a
vncnttan <vincent.tanjaya.vt@gmail.com>
2024-09-18 21:10:29 +0700

    feat: WSGI Half Integration

--- RestAPI/asgi.py
+++ RestAPI/asgi.py
@@ -9,11 +9,17 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 
 import os
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter
-
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.urls import path
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RestAPI.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            
+        )
+    )
 })
