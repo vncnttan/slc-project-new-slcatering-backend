cc782544d8f45864884690ce761baf8d867ef897
vncnttan <vincent.tanjaya.vt@gmail.com>
2024-09-20 15:04:14 +0700

    feat; ASGI

--- RestAPI/asgi.py
+++ RestAPI/asgi.py
@@ -12,6 +12,7 @@ from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.urls import path
+import RestAPI
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RestAPI.settings')
 
@@ -19,7 +20,9 @@ application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
         URLRouter(
-            
+            [
+                RestAPI.routing.websocket_urlpatterns
+            ]
         )
     )
 })
